6bb813b72f8a6798603bba8b0de1c596fae2d290
meghdad0072000 <meghdad0072000@gmail.com>
2020-01-24 18:29:40 +0330

    add questions feed

---
+++ src/app/pages/questions-feed/questions-feed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-questions-feed',
+  templateUrl: './questions-feed.component.html',
+  styleUrls: ['./questions-feed.component.scss']
+})
+export class QuestionsFeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
